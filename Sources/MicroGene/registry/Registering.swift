44805f6e8794388811bbe4faf2a91f81b93f4010
Ilya Mikhaltsou <morpheby@gmail.com>
2017-10-13 01:35:00 -0700

    Added stub Registry

---
+++ Sources/MicroGene/registry/Registering.swift
@@ -0,0 +1,12 @@
+//
+//  Registering.swift
+//  MicroGene
+//
+//  Created by Ilya Mikhaltsou on 10/13/17.
+//
+
+import Foundation
+
+public protocol Registering {
+    
+}
---
+++ Sources/MicroGene/registry/Registry.swift
@@ -0,0 +1,12 @@
+//
+//  Registry.swift
+//  MicroGene
+//
+//  Created by Ilya Mikhaltsou on 10/13/17.
+//
+
+import Foundation
+
+public class Registry: Registering {
+    
+}
