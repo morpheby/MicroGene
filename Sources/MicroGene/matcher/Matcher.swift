8ac8d7eceffa55534ce3180bef96269f16c02dcc
Ilya Mikhaltsou <morpheby@gmail.com>
2017-10-13 01:26:10 -0700

    Added stub Matcher

---
+++ Sources/MicroGene/matcher/Matcher.swift
@@ -0,0 +1,12 @@
+//
+//  Matcher.swift
+//  MicroGene
+//
+//  Created by Ilya Mikhaltsou on 10/13/17.
+//
+
+import Foundation
+
+public class Matcher: Matching {
+    
+}
---
+++ Sources/MicroGene/matcher/Matching.swift
@@ -0,0 +1,12 @@
+//
+//  Matching.swift
+//  MicroGene
+//
+//  Created by Ilya Mikhaltsou on 10/13/17.
+//
+
+import Foundation
+
+public protocol Matching {
+    
+}
