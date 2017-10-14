b5673629d51102e9ed26ca712b6601f68fbabac1
Ilya Mikhaltsou <morpheby@gmail.com>
2017-10-14 03:35:31 -0700

    Reverted ImplicitlyUnwrappedOptional conformance to Storable, made VariableBinding explicitly use ImplicitlyUnwrappedOptional

--- Sources/MicroGene/matcher/Matchable.swift
+++ Sources/MicroGene/matcher/Matchable.swift
@@ -13,14 +13,14 @@ public protocol AnyVariableBinding {
     func write<T,U>(_ value: U, to holder: inout T) where T: Matchable, U: Storable
 }
 
-public struct VariableBinding<Matcher, Variable>: AnyVariableBinding where Matcher: Matchable, Variable: Storable {
+private struct VariableBinding<Matcher, Variable> where Matcher: Matchable, Variable: Storable {
     public var path: PathExpression
 
     public var type: Any.Type {
         return Variable.self
     }
 
-    public var keyPath: WritableKeyPath<Matcher, Variable>
+    public var keyPath: WritableKeyPath<Matcher, ImplicitlyUnwrappedOptional<Variable>>
 
     public func write<T,U>(_ value: U, to holder: inout T) where T: Matchable, U: Storable {
         guard var matcher = holder as? Matcher else { preconditionFailure("Invalid holder supplied to VariableBinding") }
--- Sources/MicroGene/storage/Storable.swift
+++ Sources/MicroGene/storage/Storable.swift
@@ -11,5 +11,3 @@ public protocol Storable {
     
 }
 
-// XXX: Best expressed with a conditional conformance (which is not available yet in Swift 4.0)
-extension ImplicitlyUnwrappedOptional: Storable { }
