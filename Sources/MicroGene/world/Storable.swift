844e91ae31a411ec401862da5a62c9396d1f770e
Ilya Mikhaltsou <morpheby@gmail.com>
2017-10-14 03:01:25 -0700

    ImplicitlyUnwrappedOptional conforms to Storable (for Matchable Bindings to work)

--- Sources/MicroGene/world/Storable.swift
+++ Sources/MicroGene/world/Storable.swift
@@ -10,3 +10,6 @@ import Foundation
 public protocol Storable {
     
 }
+
+// XXX: Best expressed with a conditional conformance (which is not available yet in Swift 4.0)
+extension ImplicitlyUnwrappedOptional: Storable { }
