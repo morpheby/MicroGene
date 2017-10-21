ffe2513f6f0f36e82fb3e97c0f8b97af50b24f57
Ilya Mikhaltsou <morpheby@gmail.com>
2017-10-20 23:08:34 -0700

    GeneTests empty added

--- Tests/MicroGeneTests/GeneTest.swift
+++
@@ -1,8 +0,0 @@
-//
-//  GeneTest.swift
-//  MicroGeneTests
-//
-//  Created by Ilya Mikhaltsou on 10/20/17.
-//
-
-import Foundation
---
+++ Tests/MicroGeneTests/GeneTests.swift
@@ -0,0 +1,21 @@
+//
+//  GeneTests.swift
+//  MicroGeneTests
+//
+//  Created by Ilya Mikhaltsou on 10/20/17.
+//
+
+import Foundation
+import XCTest
+@testable import MicroGene
+
+class GeneTests: XCTestCase {
+
+    static var allTests = [
+        (),
+    ]
+}
+
+
+
+
